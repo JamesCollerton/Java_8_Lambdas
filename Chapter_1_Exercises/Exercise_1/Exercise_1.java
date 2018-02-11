fd32ca3a0454eded17d86d1e5e514ef139c904ac
James Collerton <jamescollerton@live.co.uk>
2018-02-11 15:31:57 +0000

    Got chapter one done

--- Chapter_1_Exercises/Exercise_1/Exercise_1.java
+++ Chapter_1_Exercises/Exercise_1/Exercise_1.java
@@ -1,7 +1,33 @@
+import java.util.function.Predicate;
+
 public class Exercise_1 {
 
 	public static void main(String[] args) {
+		calculatorExample();
+		inferenceExample();
+	}
+
+	public static void calculatorExample() {
 		System.out.println(Calculator.abs.apply(-1));
 	}
+
+	public static void inferenceExample() {
+
+		// Inferrable because they are both void and no type confusion
+		Runnable helloWorld = () -> System.out.println("hello world");
+
+		// Will both work	
+		checkOne(x -> x > 5);	
+		checkTwo(x -> x > 5);	
+	
+	}
+
+	public static boolean checkOne(IntPred predicate) {
+		return true;	
+	};
+
+	public static boolean checkTwo(Predicate<Integer> predicate) {
+		return true;
+	};
 
 }
---
+++ Chapter_1_Exercises/Exercise_1/IntPred.java
@@ -0,0 +1,5 @@
+public interface IntPred {
+
+	public abstract boolean test(Integer value);
+
+}
