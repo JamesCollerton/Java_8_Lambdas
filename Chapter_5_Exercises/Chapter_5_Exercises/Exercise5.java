a544f071b9f1f6a6ab16038589473ee4432d27b6
James Collerton <jamescollerton@live.co.uk>
2018-03-05 21:06:13 +0000

    Done collect and reduce

--- Chapter_5_Exercises/Chapter_5_Exercises/Exercise5.java
+++ Chapter_5_Exercises/Chapter_5_Exercises/Exercise5.java
@@ -1,7 +1,33 @@
+import java.util.stream.*;
+import java.util.*;
+
 public class Exercise5 {
 
 	public static void main(String[] args) {
-		new MethodReferences();
+		//new MethodReferences();
+		
+		longestName();
+		
+	}
+
+	public static void longestName() {
+	
+		Stream<String> namesOne = Stream.of("John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr", "Pete Best", "Stuart Sutcliffe");
+		longestNameReduce(namesOne);	
+		
+		Stream<String> namesTwo = Stream.of("John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr", "Pete Best", "Stuart Sutcliffe");
+		longestNameCollector(namesTwo);
+
+	}
+
+	public static void longestNameReduce(Stream<String> names) {
+		System.out.println(names.reduce("", (curr, nex) -> {
+			return curr.length() > nex.length() ? curr : nex;
+		}));
+	}
+	
+	public static void longestNameCollector(Stream<String> names) {
+		System.out.println(names.collect(Collectors.maxBy(Comparator.comparing(String::length))).get());
 	}
 
 }
